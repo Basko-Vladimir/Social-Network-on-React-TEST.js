60bc72c2f36b4dd3784e22291bee6e784e6d1395
BaVlaG <1989bvg@gmail.com>
2020-01-16 21:38:17 +0300

    Part 44 - Context API (StoreContext)

--- src/StoreContext.js
+++ src/StoreContext.js
@@ -1,6 +1,15 @@
 import * as React from "react";
+import App from "./App";
 
 
 const StoreContext = React.createContext(null);
+
+export const Provider = (props) => {
+    return (
+        <StoreContext.Provider value={props.store}>
+            <App/>
+        </StoreContext.Provider>
+    )
+};
 
 export default StoreContext;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
-import App from "./App";
 import React from "react";
-import StoreContext from "./StoreContext";
+import {Provider} from "./StoreContext";
 
 let rerender = () => {
     ReactDOM.render(
         <BrowserRouter>
-            <StoreContext.Provider value={store}>
-                <App/>
-            </StoreContext.Provider>
+            <Provider store={store}/>
         </BrowserRouter>, document.getElementById('root')
     )
 };
