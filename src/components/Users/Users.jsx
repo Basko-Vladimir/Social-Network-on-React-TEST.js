4ff256c09ab25f5e77dfbf83337a57294f82c586
BaVlaG <1989bvg@gmail.com>
2020-01-20 19:59:09 +0300

    Part 52 - Side Effect (Rid side effect with help button)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,35 +2,47 @@ import React from "react";
 import styles from "./Users.module.css"
 import * as axios from "axios";
 import userPhoto from "../../assets/images/user.png"
+
+
 const Users = (props) => {
+    let seyUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
+    };
 
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
-        });
-    }
-
-    return (
-        props.users.map(el =>
-            <div keys={el.id} className={styles.user}>
-                <div>
-                    <div>
-                        <img src={el.photos.small !== null ? el.photos.small : userPhoto } alt="photo" className={styles.userPhoto}/>
-                        <div>
-                            {el.followed
-                                ? <button onClick={() => {props.follow(el.id)} }>Follow</button>
-                                : <button onClick={() => {props.unfollow(el.id)} }>unFollow</button>
-                            }
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.userDescription}>
-                    <div>{el.name}</div>
-                    <div>{'el.location.country'},</div>
-                    <div>{el.status}</div>
-                    <div>{'el.location.city'}</div>
-                </div>
-            </div>)
+    return (<div>
+         <button onClick={seyUsers}> Set Users </button>
+        {
+            props.users.map(el =>
+             <div key={el.id} className={styles.user}>
+                 <div>
+                     <div>
+                         <img src={el.photos.small !== null ? el.photos.small : userPhoto} alt="photo"
+                              className={styles.userPhoto}/>
+                         <div>
+                             {el.followed
+                                 ? <button onClick={() => {
+                                     props.follow(el.id)
+                                 }}>Follow</button>
+                                 : <button onClick={() => {
+                                     props.unfollow(el.id)
+                                 }}>unFollow</button>
+                             }
+                         </div>
+                     </div>
+                 </div>
+                 <div className={styles.userDescription}>
+                     <div>{el.name}</div>
+                     <div>{'el.location.country'},</div>
+                     <div>{el.status}</div>
+                     <div>{'el.location.city'}</div>
+                 </div>
+             </div>)
+        }
+         </div>
     )
 };
 
