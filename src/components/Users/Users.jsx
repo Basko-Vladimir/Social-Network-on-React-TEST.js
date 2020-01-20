420d6f06c07548eaf241b13cd1bea5af458a3cea
BaVlaG <1989bvg@gmail.com>
2020-01-20 18:56:44 +0300

    Part 50-51 - REST API (done user API)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,21 @@
 import React from "react";
 import styles from "./Users.module.css"
-
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png"
 const Users = (props) => {
-    let newUsers = [
-        {
-            id: 1,
-            userName: 'Vladimir',
-            followed: true,
-            status: 'I\'am a president',
-            location: {country: 'Ukraine', city: 'Kiev'},
-            photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Volodymyr_Zelensky_Official_portrait.jpg/1200px-Volodymyr_Zelensky_Official_portrait.jpg'
-        },
-        {
-            id: 2,
-            userName: 'Elena',
-            followed: false,
-            status: 'I\'m a first lady',
-            location: {country: 'Ukraine', city: 'Kiev'},
-            photoURL: 'https://img.dni.ru/binaries/v3_photo/125320.jpg'
-        },
-        {
-            id: 3,
-            userName: 'Evgenii',
-            followed: true,
-            status: 'I\'m a blogger and I like football',
-            location: {country: 'Russia', city: 'Moskow'},
-            photoURL: 'https://24smi.org/public/media/celebrity/2019/03/19/crub8rogeoln-evgenii-savin.jpg'
-        }
-    ];
 
     if (props.users.length === 0) {
-       props.setUsers(newUsers);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        });
     }
-
 
     return (
         props.users.map(el =>
             <div keys={el.id} className={styles.user}>
                 <div>
                     <div>
-                        <img src={el.photoURL} alt="photo" className={styles.userPhoto}/>
+                        <img src={el.photos.small !== null ? el.photos.small : userPhoto } alt="photo" className={styles.userPhoto}/>
                         <div>
                             {el.followed
                                 ? <button onClick={() => {props.follow(el.id)} }>Follow</button>
@@ -49,10 +25,10 @@ const Users = (props) => {
                     </div>
                 </div>
                 <div className={styles.userDescription}>
-                    <div>{el.userName}</div>
-                    <div>{el.location.country},</div>
+                    <div>{el.name}</div>
+                    <div>{'el.location.country'},</div>
                     <div>{el.status}</div>
-                    <div>{el.location.city}</div>
+                    <div>{'el.location.city'}</div>
                 </div>
             </div>)
     )
